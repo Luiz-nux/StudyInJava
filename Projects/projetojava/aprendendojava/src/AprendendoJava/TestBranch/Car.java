2b158b66180c44e36c7c9d13b96a52e6eed0b263
Luiz <luizviradodev@gmail.com>
2026-07-01 20:57:32 -0300

    add test01

---
+++ Projects/projetojava/aprendendojava/src/AprendendoJava/TestBranch/Car.java
@@ -0,0 +1,13 @@
+package AprendendoJava.TestBranch;
+
+public class Car {
+    private int maxSpeed;
+    private int limitSpeed;
+
+    public Car(int maxSpeed, int limitSpeed){
+        this.maxSpeed = maxSpeed;
+        this.limitSpeed = limitSpeed;
+    }
+
+    
+}
